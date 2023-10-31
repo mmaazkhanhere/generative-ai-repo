ea62f7a6422cedf146eb2554fefebbaebc135845
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2023-10-31 16:02:00 +0500

    function online class completed

--- loop_class.py
+++ loop_class.py
@@ -5,21 +5,23 @@ l1 : list[int] = [1,2,3,4,5];
 for n in l1:
     print(f"Current number {n}");
 
-l1: tuple[int] = (1,2,3,4,5);
-for n in l1:
-    print(f"Item in tuple: {n}");
+from typing import Tuple
+l0: Tuple[int, ...] = (1, 2, 3, 4, 5)
+for n in l0:
+    print(f"Item in tuple: {n}")
 
-l1: str = "Pakistan";
-for c in l1:
+l2: str = "Pakistan";
+for c in l2:
     print(f"Current character: {c}");
 
-l1: dict[str,str] = {"name": "Maaz", "Age":'25'}; #Iteration perfom on keys
-for k in l1:
-    print(f"Dictionary key {k} and value is {l1[k]}");
+from typing import Dict
+l3: Dict[str, str] = {"name": "Maaz", "Age": '25'}  # Change '25' to 25 if 'Age' should be an integer
+for k in l3:
+    print(f"Dictionary key {k} and value is {l3[k]}")
 
 
-l1: set[int] = {1,2,3,4,1,1,2};
-for s in l1:
+l4: set[int] = {1,2,3,4,1,1,2};
+for s in l4:
     print(f"Items in set: {s}");
 
 name : str = input("What is your name: \t");
