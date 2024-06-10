36255652acc6c75dd663e9431ade9b9586a88a1e
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-06-10 13:35:58 +0500

    starting python crash course

--- panaverse-gen-ai/07-fast-api/f2.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app: FastAPI = FastAPI()
-
-@app.get("/notifications/")
-def notifications(filter: str):
-    return {"message": "filter "+filter}
--- panaverse-gen-ai/07-fast-api/header.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI, Header
-
-app: FastAPI = FastAPI()
-
-@app.post('/hi')
-def greet(who: str = Header()):
-    return f"Hello {who}"
